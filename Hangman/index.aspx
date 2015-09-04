d457e80775565b534e42ff1a7903b5de1a0427e4
Jonathan Diep <jonathandiep@hotmail.com>
2015-09-03 21:45:05 -0700

    upload

---
+++ Hangman/index.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Hangman.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Assignment 4 | Hangman</title>
+    <link rel="stylesheet" href="CSS/bootstrap.min.css" />
+    <link rel="stylesheet" href="CSS/hangman.css" />
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Hangman</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="row">
+        <form id="form1" runat="server">
+        <div class="col-md-3 col-md-offset-1">
+            <asp:RadioButtonList ID="rblist" runat="server">
+                <asp:ListItem Text="Easy" Selected="True" Value="easy" />
+                <asp:ListItem Text="Medium" Value="medium" />
+                <asp:ListItem Text="Hard" Value="hard" />
+            </asp:RadioButtonList>
+            <asp:Button ID="difficultyButton" class="btn btn-sm btn-primary" Text=" Select Difficulty " OnClick="change_Difficulty" runat="server" />
+        </div>
+        <div class="col-md-3 col-md-offset-1">
+            <asp:Image ID="hangman" runat="server" ImageUrl="Images/0.gif" />
+            <asp:Label ID="hide" runat="server" />
+            <br />
+            <asp:Label ID="round" runat="server" />
+        </div>
+        <div class="col-md-3 col-md-offset-1">
+            <asp:TextBox ID="userInput" placeholder="Guess Here..." runat="server" />
+            <asp:Button text=" Submit " class="btn btn-sm btn-primary" ID="submit" OnClick="submit_Click" runat="server" />
+            <br />
+            <asp:Label ID="statusLabel" runat="server" />
+        </div>
+        </form>
+    </div>
+    <script src="JS/bootstrap.min.js"></script>
+    <script src="JS/jquery-1.11.3.min.js"></script>
+</body>
+</html>
---
+++ Hangman/index.aspx.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Hangman
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            hide.Text = HiddenAnswer;
+            hangman.ImageUrl = "Images/" + Guesses.ToString() + ".gif";
+            round.Text = "Guesses Left: " + (6 - Guesses);
+        }
+
+        protected string Answer
+        {
+            get
+            {
+                if (Session["Answer"] == null)
+                    Session["Answer"] = GetRandomAnswer();
+                return (string)Session["Answer"];
+            }
+        }
+
+        protected string HiddenAnswer
+        {
+            get
+            {
+                if (Session["HiddenAnswer"] == null)
+                {
+                    foreach (var letter in Answer)
+                    {
+                        Session["HiddenAnswer"] += "*";
+                    }
+                }
+                return (string)Session["HiddenAnswer"];
+            }
+        }
+
+        protected string UserEntry
+        {
+            get
+            {
+                Session["UserEntry"] = userInput.Text;
+                return (string)Session["UserEntry"];
+            }
+        }
+
+        protected string Difficulty
+        {
+            get
+            {
+                Session["Difficulty"] = rblist.SelectedValue;
+                return (string)Session["Difficulty"];
+            }
+        }
+
+        protected int Guesses
+        {
+            get
+            {
+                if (Session["Guesses"] == null)
+                {
+                    if (Difficulty == "hard")
+                    {
+                        Session["Guesses"] = 4;
+                    }
+                    else if (Difficulty == "medium")
+                    {
+                        Session["Guesses"] = 2;
+                    }
+                    else
+                    {
+                        Session["Guesses"] = 0;
+                    }
+                }
+                return (int)Session["Guesses"];
+            }
+            set
+            {
+                Session["Guesses"] = value;
+            }
+
+        }
+
+        private string GetRandomAnswer()
+        {
+            string dataAnswers;
+            if (Difficulty == "easy")
+            {
+                dataAnswers = File.ReadAllText(Server.MapPath("~/Answers/easy.txt"));
+            }
+            else if (Difficulty == "medium")
+            {
+                dataAnswers = File.ReadAllText(Server.MapPath("~/Answers/medium.txt"));
+            }
+            else
+            {
+                dataAnswers = File.ReadAllText(Server.MapPath("~/Answers/hard.txt"));
+            }
+
+            List<string> answers = dataAnswers.Split(',').ToList();
+            Random rand = new Random();
+            string word = answers[rand.Next(0, answers.ToArray().Length)];
+            return word;
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            statusLabel.ForeColor = System.Drawing.Color.Black;
+            statusLabel.Font.Bold = false;
+            if (Guesses == 5)
+            {
+                statusLabel.Text = "YOU LOSE";
+                Guesses++;
+                statusLabel.ForeColor = System.Drawing.Color.Red;
+                statusLabel.Font.Bold = true;
+            }
+            else if(userInput.Text.Length > 1)
+            {
+                if (userInput.Text.ToUpper() == Answer.ToUpper())
+                {
+                    Session["HiddenAnswer"] = Answer;
+                    statusLabel.Text = "YOU WIN";
+                    statusLabel.ForeColor = System.Drawing.Color.Green;
+                    statusLabel.Font.Bold = true;
+                }
+                else
+                {
+                    statusLabel.Text = "ur bad at guessing lol";
+                    Guesses++;
+                }
+            }
+            else
+            {
+                if (userInput.Text == "")
+                {
+                    statusLabel.Text = "u didn't fill anything but you submitted so u lose a point lol";
+                        Guesses++;
+                }
+                else if(Answer.Contains(userInput.Text))
+                {
+                    statusLabel.Text = "good guess";
+                    List<string> savedAnswer = new List<string>();
+                    foreach (char letter in HiddenAnswer)
+                    {
+                        savedAnswer.Add(letter.ToString());
+                    }
+                    List<string> ans = new List<string>();
+                    foreach (char letter in Answer)
+                    {
+                        if (userInput.Text == letter.ToString())
+                        {
+                            ans.Add(letter.ToString());
+                        }
+                        else
+                        {
+                            ans.Add("*");
+                        }
+                    }
+                    List<string> compiledAnswer = new List<string>();
+                    string[] savedAnswerArray = savedAnswer.ToArray();
+                    string[] ansArray = ans.ToArray();
+                    for (var i = 0; i < ansArray.Length; i++)
+                    {
+                        if (savedAnswer[i] == ans[i])
+                        {
+                            compiledAnswer.Add("*");
+                        }
+                        else
+                        {
+                            if (savedAnswer[i] == "*")
+                            {
+                                compiledAnswer.Add(ans[i]);
+                            }
+                            else
+                            {
+                                compiledAnswer.Add(savedAnswer[i]);
+                            }
+                        }
+                    }
+                    string output = string.Join("", compiledAnswer.ToArray());
+                    Session["HiddenAnswer"] = output;
+                }
+                else
+                {
+                    statusLabel.Text = "ur bad at guessing lol";
+                    Guesses++;
+                }
+            }
+            if (HiddenAnswer == Answer)
+            {
+                statusLabel.Text = "YOU WIN";
+            }
+            userInput.Text = string.Empty;
+            Page_Load(null, EventArgs.Empty);
+        }
+
+        protected void change_Difficulty(object sender, EventArgs e)
+        {
+            Session["Answer"] = GetRandomAnswer();
+            Session["HiddenAnswer"] = null;
+            Session["Difficulty"] = null;
+            Session["Guesses"] = null;
+            Session["ComparedVariable"] = null;
+            statusLabel.Text = string.Empty;
+            userInput.Text = string.Empty;
+            Page_Load(null, EventArgs.Empty);
+        }
+    }
+}
